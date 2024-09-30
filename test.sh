bf8e47af3d13c658ed948f401a49b9102a86fab6
Michel Pelletier <michel@onesparse.com>
2024-09-29 17:47:58 -0700

    tweak test runner.

--- test.sh
+++ test.sh
@@ -15,14 +15,12 @@ set -e
 echo building test image
 docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) . -t onesparse/test
 
-echo running test container
 docker run --user $(id -u):$(id -g) --mount type=bind,source=$(pwd),target=/home/postgres/onesparse,bind-propagation=rshared --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -d --name "$DB_HOST" onesparse/test
 
 $EXECIT pg_ctl start
 # $EXEC make clean
 # $EXEC make install
 
-echo waiting for database to accept connections
 until
     $EXEC \
 	    psql -o /dev/null -t -q -U "$SU" \
@@ -32,7 +30,5 @@ do sleep 1;
 done
 
 $EXEC make installcheck
-
-echo destroying test container and image
 
 docker rm --force "$DB_HOST"
