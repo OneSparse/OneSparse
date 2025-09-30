0f2d13c291990d54ebf13a639052e1613c973926
Michel Pelletier <pelletier.michel@gmail.com>
2025-09-30 14:30:31 -0700

    io tests.

--- sql/test_io_header.sql
+++ sql/test_io_header.sql
@@ -1,8 +1,28 @@
--- # IO Operations
+-- # Input/Output Operations
 --
--- select serialize(random_matrix(10, 10, 10, seed=>0.42, max=>99)) as matrixbytes \gset
+-- OneSparse comes with several functions to read and write different
+-- text and binary formats for graph data.
+--
+-- ## SuiteSparse Binaries
+--
+-- SuiteSparse provides a very efficient compressed binary
+-- representation for matrix and vector objects.
+
 \set grbfile 'testmatrix.grb'
-select serialize(random_matrix(10, 10, 10, seed=>0.42, max=>99)) as matrixbytes \gset
-select deserialize(:'matrixbytes') = random_matrix(10, 10, 10, seed=>0.42, max=>99);
-select serialize_file(random_matrix(10, 10, 10, seed=>0.42, max=>99), :'grbfile');
-select deserialize_file(:'grbfile') = random_matrix(10, 10, 10, seed=>0.42, max=>99);
+
+select serialize(random_matrix('int8', 10, 10)) as matrixbytes \gset
+select deserialize(:'matrixbytes') = random_matrix('int8', 10, 10);
+
+select serialize_file(random_matrix('int8', 10, 10), :'grbfile');
+select deserialize_file(:'grbfile') = random_matrix('int8', 10, 10);
+
+-- ## MatrixMarket Format
+--
+-- The Matrix Market format is a standard graph distribution format
+-- used by the SuiteSparse Matrix Library.  OneSparse can read these
+-- files and return matrix objects.
+
+-- ## Large Object Storage
+--
+-- Matrices bigger than the 1GB TOAST limit can be stored in the Large
+-- Object table in Postgres, which supports storage up to 4TB.
